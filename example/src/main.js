4031f32c57456f3c99927318d72815f4cb5fadf9
MAC <mac@MACdeMacBook-Air.local>
2022-07-27 00:40:21 +0800

    引用使用正常

--- example/src/main.js
+++ example/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import AntDesignVue from 'ant-design-vue'
+Vue.use(AntDesignVue)
 
 /* eslint-disable */
 import AdvtUI from 'advt-ui'
