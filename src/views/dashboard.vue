fbf4adf89fb49c5c68cf00113865d7a5bcf61819
xiejunping <xiejunping@cnhnkj.com>
2022-11-11 09:38:46 +0800

    增加跳转路由

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -3,7 +3,9 @@
     <h1>演示组件目录</h1>
 
     <a-row :gutter="20">
-      <a-col :span="8"></a-col>
+      <a-col :span="8">
+        <router-link to="/drag-weektime">Drag-Weektime</router-link>
+      </a-col>
       <a-col :span="8"></a-col>
       <a-col :span="8"></a-col>
     </a-row>
